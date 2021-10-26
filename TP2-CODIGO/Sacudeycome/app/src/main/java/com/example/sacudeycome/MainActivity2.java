6477114c835cd1d73b58c34e7631d040bcfe6bcf
ezezella <ezezella@gmail.com>
2021-10-25 21:19:36 -0300

    Shake template was added

---
+++ TP2-CODIGO/Sacudeycome/app/src/main/java/com/example/sacudeycome/MainActivity2.java
@@ -0,0 +1,66 @@
+package com.example.sacudeycome;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+    private TextView tvShake;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {   //demo link   http://jasonmcreynolds.com/?p=388
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tvShake = findViewById(R.id.tvShake);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,ServiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake(int count) {
+                tvShake.setText("Shake Action is just detected!!");
+                Toast.makeText(MainActivity2.this, "Shaked!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+}
--- TP2-CODIGO/Sacudeycome/app/src/main/java/com/example/sacudeycome/SelectorActivity.java
+++ TP2-CODIGO/Sacudeycome/app/src/main/java/com/example/sacudeycome/SelectorActivity.java
@@ -1,79 +1,122 @@
 package com.example.sacudeycome;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.net.URI;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class SelectorActivity extends AppCompatActivity {
 
     private static String menuPath = "Menu.txt";
-    private ArrayList<String[]> listaCampos = new ArrayList<String[]>();
+    private ArrayList<String[]> listaMenus = new ArrayList<String[]>();
 
     private TextView titulo;
     private TextView descripcion;
     private TextView precio;
     private ImageView imagen;
 
+
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
         Log.d("Estoy en el archivo buscando","Sigo buscando");
-        leerArchivo();
+//        leerArchivo();
+        cargarLista();
         titulo = findViewById(R.id.titulo);
         descripcion = findViewById(R.id.desc);
         precio = findViewById(R.id.precio);
         imagen = findViewById(R.id.imagen);
         cargarMenu(0);
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
     }
 
     public void cargarMenu (int nunMenu){
         Log.d("LLega aca?","LLEGA ACA!");
 
-        String[] campos=listaCampos.get(nunMenu);
+        String[] campos=listaMenus.get(nunMenu);
         titulo.setText(campos[0]);
         descripcion.setText(campos[2]);
         precio.setText(campos[1]);
-        imagen.setImageURI(Uri.parse(menuPath+"/"+campos[3]));
+
+//        switch (nunMenu) {
+//            case 0:
+//                imagen.setImageResource(R.drawable.muzzarella);
+//                break;
+//            case 1:
+//                imagen.setImageResource(R.drawable.huevo);
+//                break;
+//            case 2:
+//                imagen.setImageResource(R.drawable.fugazzetta);
+//                break;
+//            case 3:
+//                imagen.setImageResource(R.drawable.fugazzetta_con_panceta);
+//                break;
+//            case 4:
+//                imagen.setImageResource(R.drawable.jamon);
+//                break;
+//            case 5:
+//                imagen.setImageResource(R.drawable.jamon_y_morrones);
+//                break;
+//            case 6:
+//                imagen.setImageResource(R.drawable.napolitana);
+//                break;
+//            case 7:
+//                imagen.setImageResource(R.drawable.calabresa);
+//                break;
+//            case 8:
+//                imagen.setImageResource(R.drawable.roquefort);
+//                break;
+//        }
+//        imagen.setImageURI(Uri.parse(menuPath+"/"+campos[3]));
     }
 
 
-
+    public void cargarLista(){
+        listaMenus.add(("Muzzarella-460-Salsa de tomate, muzzarella, aceitunas").split("-"));
+        listaMenus.add(("Huevo-500-Salsa de tomate, muzzarella, huevo duro, oregano, aceitunas").split("-"));
+        listaMenus.add(("Fugazzetta-500-Cebolla, muzzarella, adobo p/pizza, aceitunas").split("-"));
+        listaMenus.add(("Fugazzeta con panceta-550-Muzzarella, cebolla, panceta, adobo p/pizza, aceitunas").split("-"));
+        listaMenus.add(("Jamon-550-Salsa de tomate, muzzarella, jamon, oregano, aceitunas").split("-"));
+        listaMenus.add(("Jamon y morrones-600-Salsa de tomate, muzzarella, jamon, morrones asados, oregano, aceitunas").split("-"));
+        listaMenus.add(("Napolitana-510-Salsa de tomate, muzzarella, tomate en rodajas, provenzal, aceitunas").split("-"));
+        listaMenus.add(("Calabresa-610-Salsa de tomate, muzzarella,longaniza, morron asado en tiras, oregano, aceitunas").split("-"));
+        listaMenus.add(("Roquefort-610-Salsa de tomate, muzzarella, roquefort, oregano, aceitunas").split("-"));
+    }
 
     public void leerArchivo (){
             File archivo = null;
             FileReader fr = null;
             BufferedReader br = null;
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            //Verifica permisos para Android 6.0+
-            int permissionCheck = ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-                Log.i("Mensaje", "No se tiene permiso para leer.");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 225);
-            } else {
-                Log.i("Mensaje", "Se tiene permiso para leer!");
-            }
-        }
             try {
-                archivo = new File(menuPath);
+                archivo = new File("Menu.txt");
                 fr = new FileReader(archivo);
                 br = new BufferedReader(fr);
 
@@ -82,7 +125,7 @@ public class SelectorActivity extends AppCompatActivity {
                     Log.d("Linea: ", linea);
                     String[] campos = linea.split("-");
                     Log.d("Linea: ", campos[0]);
-                    listaCampos.add(campos);
+                    listaMenus.add(campos);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -96,5 +139,39 @@ public class SelectorActivity extends AppCompatActivity {
                 }
             }
     }
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
+
+
+
 }
+
 //Titulo-Precio-Descripcion-IdImagen
